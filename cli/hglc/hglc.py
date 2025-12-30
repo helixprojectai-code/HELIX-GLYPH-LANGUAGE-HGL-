e240cd2cfa76f94f889bd4bbc13d835aeafabbb4
Stephen Hope <53706288+stephenh67@users.noreply.github.com>
2025-12-30 11:42:16 -0500

    Update hglc.py

--- cli/hglc/hglc.py
+++ cli/hglc/hglc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys, json, hashlib, argparse
+
 ALLOWED_INTENT={"approve","deny","request"}
 ALLOWED_ACT={"access","upsert","execute"}
 ALLOWED_KIND={"Human","IAD","CLS"}
